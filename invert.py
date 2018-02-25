21cf2a5b207802eb0fefc5b58801b16c980dd773
Kallirroi <kallirroi.retzepi@gmail.com>
2018-02-25 17:46:36 -0500

    inverting image

---
+++ invert.py
@@ -0,0 +1,25 @@
+import cv2;
+import numpy as np
+ 
+# Read image
+im_in = cv2.imread("out/screenshot_pixelated.png", cv2.IMREAD_GRAYSCALE);
+cv2.imwrite('out/screenshot_pixelated_gray.png', im_in)
+
+
+th, im_th = cv2.threshold(im_in, 255, 255, cv2.THRESH_TOZERO_INV);
+ 
+# Copy the thresholded image.
+im_floodfill = im_th.copy()
+ 
+# Mask used to flood filling.
+h, w = im_th.shape[:2]
+mask = np.zeros((h+2, w+2), np.uint8)
+ 
+# Floodfill from point (0, 0)
+cv2.floodFill(im_floodfill, mask, (0,0), 255);
+ 
+# Invert floodfilled image
+im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+# Save
+cv2.imwrite('out/screenshot_pixelated_gray_inverted.png', im_floodfill_inv)
--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ echo "preparing to pixelate"
 python pixelate.py
 echo "moving image to out/ folder"
 mv in/screenshot_pixelated.png out/
-echo "graying final output"
-python gray.py
+# echo "graying final output"
+# python gray.py
+echo "graying and inverting"
+python invert.py
 echo "done"
